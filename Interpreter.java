963c4ebb9dd1240b633b1c27034f22ac4f17d003
samiiin <47495714+samiiin@users.noreply.github.com>
2019-02-10 13:56:06 +0330

    Update Interpreter.java

--- Interpreter.java
+++ Interpreter.java
@@ -1,17 +1,21 @@
 package whilelang.interp;
-
+import java.util.*;
 import whilelang.ast.*;
 
 public class Interpreter implements Visitor<Integer>{
+	public HashMap<String, Integer> variables = new HashMap<String, Integer>();
 
 	public Integer visit(Print n) {
 		// TODO Implement this!
+		String expression=n.msg+Integer.toString(variables.get(n.varID));
+		System.out.println(expression);
 		return null;
 	}
 
 	public Integer visit(Assign n) {
 		// TODO Implement this!
-		return null;
+		variables.put(n.varID,n.expr.accept(this));
+		return n.expr.accept(this);
 	}
 
 	public Integer visit(Skip n) {
@@ -21,28 +25,40 @@ public class Interpreter implements Visitor<Integer>{
 
 	public Integer visit(Block n) {
 		// TODO Implement this!
+		for( Statement stat: n.body)
+			stat.accept(this);
 		return null;
 	}
 
 	public Integer visit(IfThenElse n) {
 		// TODO Implement this!
-		return null;
+		if((n.expr.accept(this)==1))
+			return n.then.accept(this);
+		else
+			return n.elze.accept(this);
 	}
 
 	public Integer visit(While n) {
 		// TODO Implement this!
+		while(n.expr.accept(this)!=0)
+			n.body.accept(this);
 		return null;
 	}
 
 	public Integer visit(For n) {
 		// TODO Implement this!
+		n.init.accept(this);
+		while(n.expr.accept(this)!=0){
+			n.body.accept(this);
+			n.step.accept(this);
+		}
 		return null;
 	}
 
 
 	public Integer visit(Var n) {
 		// TODO Implement this!
-		return null;
+		return variables.get(n.varID);
 	}
 
 	public Integer visit(IntLiteral n) {
@@ -52,62 +68,80 @@ public class Interpreter implements Visitor<Integer>{
 
 	public Integer visit(Plus n) {
 		// TODO Implement this!
-		return n.lhs.accept(this) + n.rhs.accept(this);
+		return n.lhs.accept(this)+n.rhs.accept(this);
 	}
 
 	public Integer visit(Minus n) {
 		// TODO Implement this!
-		return n.lhs.accept(this) - n.rhs.accept(this);
+		return n.lhs.accept(this)-n.rhs.accept(this);
 	}
 
 	public Integer visit(Times n) {
 		// TODO Implement this!
-		return n.lhs.accept(this) * n.rhs.accept(this);
+		return n.lhs.accept(this)*n.rhs.accept(this);
 	}
 
 	public Integer visit(Division n) {
 		// TODO Implement this!
-		return n.lhs.accept(this) / n.rhs.accept(this);
+		return n.lhs.accept(this)/n.rhs.accept(this);
 	}
 
 	public Integer visit(Modulo n) {
 		// TODO Implement this!
-		return n.lhs.accept(this) % n.rhs.accept(this);
+		return n.lhs.accept(this)%n.rhs.accept(this);
 	}
 
 	public Integer visit(Equals n) {
 		// TODO Implement this!
-		return null;
+		if(n.lhs.accept(this)==n.rhs.accept(this))
+			return 1;
+		else
+			return 0;
 	}
 
 	public Integer visit(GreaterThan n) {
 		// TODO Implement this!
-		return null;
+		if(n.lhs.accept(this)>n.rhs.accept(this))
+			return 1;
+		else
+			return 0;
 	}
 
 	public Integer visit(LessThan n) {
 		// TODO Implement this!
-		return null;
+		if(n.lhs.accept(this)<n.rhs.accept(this))
+			return 1;
+		else
+			return 0;
 	}
 
 	public Integer visit(And n) {
 		// TODO Implement this!
-		return null;
+		if((n.lhs.accept(this)!=0) && (n.rhs.accept(this)!=0))
+			return 1;
+		else
+			return 0;
 	}
 
 	public Integer visit(Or n) {
 		// TODO Implement this!
-		return null;
+		if((n.lhs.accept(this)!=0) || (n.rhs.accept(this)!=0))
+			return 1;
+		else
+			return 0;
 	}
 
 	public Integer visit(Neg n) {
 		// TODO Implement this!
-		return null;
+		return -1*(n.expr.accept(this));
 	}
 
 	public Integer visit(Not n) {
 		// TODO Implement this!
-		return null;
+		if((n.expr.accept(this)==0))
+			return 1;
+		else
+			return 0;
 	}
 
 	public Integer visit(UnaryExpression n) {
